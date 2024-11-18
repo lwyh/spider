3989235af92f80fba84589d51be1e1618961f3e9
lwyh <lwyhnice@gmail.com>
2024-11-18 23:18:58 +0800

    yuangongpaiqian

---
+++ huawei/yuangongpaiqian.py
@@ -0,0 +1,13 @@
+"""
+员工派遣规则：代号x的倍数不能去x国家，找到最小的k满足的连续的员工号满足所有国家派遣的规则
+"""
+
+
+
+def fenpai():
+    arr=list(map(int,input().split()))
+    x,y,cntx,cnty=arr[0],arr[1],arr[2],arr[3]
+
+    while l>=cntx+cnty:
+        
+    
