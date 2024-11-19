4a70b9da3cb7f07d513aa15cea459dac0e5d3f36
lwyh <lwyhnice@gmail.com>
2024-11-19 12:32:26 +0800

    20241120

--- huawei/yuangongpaiqian.py
+++ huawei/yuangongpaiqian.py
@@ -8,6 +8,29 @@ def fenpai():
     arr=list(map(int,input().split()))
     x,y,cntx,cnty=arr[0],arr[1],arr[2],arr[3]
 
+    
+    listx=[]
+    listy=[]
+    l=cntx+cnty
+    for i in range(l):
+        if((i+1) % x!=0):
+            listx.append((i+1))
+        else:
+            if((i+1) % y!=0):
+                listy.append((i+1))
     while l>=cntx+cnty:
+        if(len(listx)>=cntx and len(listy)>=cnty):
+            print(l)
+            break
+        else:
+            l+=1
+            if(l%x!=0):
+                listx.append(l)
+            if(l%y !=0):
+                listy.append(l)
+    return l
+
+if __name__=="__main__":
+    fenpai()
         
     
